8fc82601392c89352069a28836802c4399360d65
Fernando Ulloa <fulloamoreno@gmail.com>
2024-04-02 15:26:39 -0600

    Presentación terminada

---
+++ lib/screens/school.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import '../widgets/widgets.dart';
+import 'package:go_router/go_router.dart';
+
+class Escuela extends StatelessWidget {
+  const Escuela({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        appBar: CustomAppBar1(),
+        body: ,
+      ),
+    );
+  }
+}
